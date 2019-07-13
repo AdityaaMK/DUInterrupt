fe3e634644be83ee8069097e0fac5f1f1c9e40cc
AdityaaMK <44482076+AdityaaMK@users.noreply.github.com>
2019-07-13 09:23:21 -0400

    Update InterfaceController.swift

--- DUInterrupt WatchKit Extension/InterfaceController.swift
+++ DUInterrupt WatchKit Extension/InterfaceController.swift
@@ -2,8 +2,8 @@
 //  InterfaceController.swift
 //  DUInterrupt WatchKit Extension
 //
-//  Created by Aditya Bose on 2/24/19.
-//  Copyright © 2019 Aditya Bose. All rights reserved.
+//  Created by Adityaa Magesh Kumar on 2/24/19.
+//  Copyright © 2019 Adityaa Magesh Kumar. All rights reserved.
 //
 
 import WatchKit
