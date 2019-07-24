c367243cf20961b880068f60fccc536c4e7bebf2
AdityaaMK <44482076+AdityaaMK@users.noreply.github.com>
2019-07-24 07:45:58 -0400

    Update WorkoutsViewController.swift

--- Application/WorkoutsViewController.swift
+++ Application/WorkoutsViewController.swift
@@ -2,7 +2,7 @@
 //  WorkoutsViewController.swift
 //  Cardiologic
 //
-//  Created by Adityaa Magesh Kumar on 8/1/19.
+//  Created by Adityaa Magesh Kumar on 7/1/19.
 //  Copyright © 2019 Adityaa Magesh Kumar. All rights reserved.
 //
 
